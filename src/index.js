22462274fa285a032d6b4beedf879ed313ad19d9
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-01-21 11:44:58 -0600

    Added Nav Bar and set up page routing to separate site content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 import { Kennel } from "./components/Kennel.js"
 import './index.css';
 // import App from './App';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Kennel />
+    <Router>
+      <Kennel />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
